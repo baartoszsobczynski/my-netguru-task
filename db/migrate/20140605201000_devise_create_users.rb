19529a4b67938d81f2cdedb3d156f2e5ed2ee658
Bartosz Sobczynski <baartoszsobczynski@gmail.com>
2016-01-12 11:43:03 +0100

    Add firstname and lastname to user

--- db/migrate/20140605201000_devise_create_users.rb
+++ db/migrate/20140605201000_devise_create_users.rb
@@ -1,6 +1,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+
+      ## User details
+      t.string :firstname,               :null => false
+      t.string :lastname,                :null => false
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -35,6 +40,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email,                unique: true
+    add_index :users, :firstname
+    add_index :users, :lastname
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
